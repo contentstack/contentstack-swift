6c1daf913b33fa3b8b76150648fe70e28509a805
Uttam K Ukkoji <uttamukkoji@gmail.com>
2020-04-02 18:21:20 +0530

    Image Transform additional Functionlity added
    Asset Protocol changes added
    Image Operation file Created
    String Url with Transform functionality added
    Transform Functionality and test case added for following:
     - auto
     - quality
     - format
     - resize
     - crop
     - canvas
     - fit
     - trim
     - orientation
     - overlay
     - overlay-align
     - overlay-repeat
     - overlay width and height
     - overlay padding
     - padding

--- Sources/ImageTransform.swift
+++ Sources/ImageTransform.swift
@@ -6,186 +6,97 @@
 //
 
 import Foundation
-
 /// The Image Delivery API is used to retrieve, manipulate and/or convert image
 /// files of your Contentstack account and deliver it to your web or mobile properties
 /// See [Image Delivery API](https://www.contentstack.com/docs/developers/apis/image-delivery-api)
-public enum ImageTransform {
+public class ImageTransform {
+    internal var imageOperation = [ImageOperation]()
 
     ///The `auto` parameter lets you enable the functionality that automates certain image optimization features.
     ///See [Automate Optimization](https://www.contentstack.com/docs/developers/apis/image-delivery-api/#automate-optimization)
-    case auto
+    public func auto() -> ImageTransform {
+        self.imageOperation.append(.auto)
+        return self
+    }
+
     ///The `quality` parameter lets you control the compression level of images that have Lossy file format.
     ///The value for this parameters can be entered in any whole number (taken as a percentage) between 1 and 100.
     ///See [Control Quality](https://www.contentstack.com/docs/developers/apis/image-delivery-api/#control-quality)
-    case qualiy(UInt)
+    public func qualiy(_ quality: UInt) -> ImageTransform {
+        self.imageOperation.append(.qualiy(quality))
+        return self
+    }
+
     ///The `format` parameter lets you converts a given image from one format to another.
     ///See [Convert Formats](https://www.contentstack.com/docs/developers/apis/image-delivery-api/#convert-formats)
-    case format(Format)
+    public func format(_ format: Format) -> ImageTransform {
+        self.imageOperation.append(.format(format))
+        return self
+    }
+
     ///The `width` parameter lets you dynamically resize the width of the image by specifying pixels or percentage.
-    ///The `height` parameter lets you dynamically resize the height of the image by specifying pixels or percentage..
+    ///The `height` parameter lets you dynamically resize the height of the image by specifying pixels or percentage.
     ///The `disable` parameter disables the functionality that is enabled by default.
     ///See [Resize Images](https://www.contentstack.com/docs/developers/apis/image-delivery-api/#resize-images)
-    case resize(Resize)
+    public func resize(_ resize: Resize) -> ImageTransform {
+        self.imageOperation.append(.resize(resize))
+        return self
+    }
+
     ///The `crop` parameter allows you to remove pixels from an image.
     ///See [Crop Images](https://www.contentstack.com/docs/developers/apis/image-delivery-api/#crop-images)
-    case crop(Crop)
-
-    internal func urlQueryItem() throws -> [URLQueryItem] {
-        switch self {
-        case .auto:
-            return [URLQueryItem(name: ImageParameter.auto, value: "webp")]
-        case .format(let format):
-            return [URLQueryItem(name: ImageParameter.fromat, value: format.value)]
-        case .qualiy(let quality) where quality > 0 && quality <= 100:
-            return [URLQueryItem(name: ImageParameter.quality, value: String(quality))]
-        case .qualiy:
-            let message = """
-            The value for Quality parameters can be entered in
-            any whole number (taken as a percentage) between 1 and 100.
-            """
-            throw ImageTransformError(message: message)
-        case .resize(let resize):
-            return resize.urlQueryItem()
-        case .crop(let crop):
-            return try crop.urlQueryItem()
-        }
+    public func crop(_ crop: Crop) -> ImageTransform {
+        self.imageOperation.append(.crop(crop))
+        return self
     }
-}
 
-public struct Resize {
-    internal let size: Size
-    internal let disableUpscale: Bool = false
-    internal func urlQueryItem() -> [URLQueryItem] {
-        var queryItems = [URLQueryItem]()
-        size.urlQueryItem(queryItems: &queryItems)
-        if disableUpscale {
-            queryItems.append(URLQueryItem(name: ImageParameter.disable, value: "upscale"))
-        }
-        return queryItems
+    ///The `canvas` parameter allows you to increase the size of the canvas that surrounds an image.
+    ///See [Canvas](https://www.contentstack.com/docs/developers/apis/image-delivery-api/#canvas)
+    public func canvas(_ canvas: Canvas) -> ImageTransform {
+        self.imageOperation.append(.canvas(canvas))
+        return self
     }
-}
-
-public struct Size {
-    public var height: UInt?
-    public var width: UInt?
-
-    internal func urlQueryItem(queryItems: inout [URLQueryItem]) {
-        if let height = height {
-            queryItems.append(URLQueryItem(name: ImageParameter.height, value: String(height)))
-        }
-        if let width = width {
-            queryItems.append(URLQueryItem(name: ImageParameter.width, value: String(width)))
-        }
+    ///The `fit` parameter enables you to fit the given image properly within the specified `height` and `width`.
+    ///You need to provide values for the `height`, `width` and `fit` parameters.
+    ///See [Fit Mode](https://www.contentstack.com/docs/developers/apis/image-delivery-api/#fit-mode)
+    public func fit(_ fit: Fit) -> ImageTransform {
+        self.imageOperation.append(.fit(fit))
+        return self
     }
-}
-
-public enum Mode {
-    case none
-    ///You can append the safe parameter when cropping an image.
-    case safe
-    ///You can also specify the smart parameter to crop a given image using content-aware algorithms
-    case smart
-
-    internal var value: String? {
-        switch self {
-        case .none:                     return nil
-        case .safe:                     return "safe"
-        case .smart:                    return "smart"
-        }
+    ///The `trim` parameter lets you trim an image from the edges.
+    ///See [Trim Images](https://www.contentstack.com/docs/developers/apis/image-delivery-api/#trim-images)
+    public func trim(_ edgeInset: NSEdgeInsets) -> ImageTransform {
+        self.imageOperation.append(.trim(edgeInset))
+        return self
     }
-}
-
-public enum Format {
-    ///Progressive JPEG Format
-    case pjpg
-    /// JPEG format
-    case jpeg
-    /// GIF format
-    case gif
-    /// PNG format
-    case png
-    /// WEBP format
-    case webp
-    /// WEBP Lossy format
-    case webply
-    /// WEBP Lossless format
-    case webpll
-
-    internal var value: String {
-        switch self {
-        case .jpeg:                     return "jpg"
-        case .pjpg:                     return "pjpg"
-        case .gif:                      return "gif"
-        case .png:                      return "png"
-        case .webp:                     return "webp"
-        case .webply:                   return "webply"
-        case .webpll:                   return "webpll"
-        }
+    ///The `orient` parameter lets you control the cardinal orientation of the given image.
+    ///See [Reorient Images](https://www.contentstack.com/docs/developers/apis/image-delivery-api/#reorient-images)
+    public func orient(_ orient: Orientation) -> ImageTransform {
+        self.imageOperation.append(.orient(orient))
+        return self
     }
-}
+    ///The `overlay` parameter allows you to put one image on top of another.
+    ///You need to specify the relative URL of the image as value for this parameter.
+    ///See[Overlay Settings](https://www.contentstack.com/docs/developers/apis/image-delivery-api/#overlay-settings)
+    public func overlay(relativeUrl: String, overlayTypes: [OverlayType] = []) -> ImageTransform {
+        self.imageOperation.append(.overlay(relativeUrl, overlayTypes))
+        return self
 
-public enum Crop {
-    ///Crop by width and height
-    case `default`(width: UInt, height: UInt)
-    ///Crop by aspect ratio
-    case aspectRatio (Size, ratio: String, mode: Mode = .none)
-    ///Crop sub region
-    case region(width: UInt, height: UInt, xRegion: UInt, yRegion: UInt, mode: Mode = .none)
-    ///Crop and offset
-    case offset(width: UInt, height: UInt, xOffset: UInt, yOffset: UInt, mode: Mode = .none)
-
-    internal func urlQueryItem() throws -> [URLQueryItem] {
-        switch self {
-        case .default(let size):
-            return [URLQueryItem(name: ImageParameter.crop, value:
-                [
-                    String(size.width),
-                    String(size.height)
-                    ].joined(separator: ",") )]
-
-        case .aspectRatio(let sizes, let ratio, let mode):
-            var queryItems = [URLQueryItem]()
-            sizes.urlQueryItem(queryItems: &queryItems)
-
-            if queryItems.count == 0 {
-                let message = """
-                Along with the crop parameter aspect-ration,
-                you also need to specify either the width or height parameter or both
-                in the API request to return an output image with the correct dimensions.
-                """
-                throw ImageTransformError(message: message)
-            }
-            var values = [ratio]
-            if let value = mode.value {
-                values.append(value)
-            }
-            queryItems.append(URLQueryItem(name: ImageParameter.crop, value: values.joined(separator: ",")))
-            return queryItems
-        case .region(let region):
-            var values = [
-                String(region.width),
-                String(region.height),
-                "x\(String(region.xRegion))",
-                "y\(String(region.yRegion))"
-            ]
-            if let value = region.mode.value {
-                values.append(value)
-            }
-            return [URLQueryItem(name: ImageParameter.crop, value: values.joined(separator: ","))]
-
-        case .offset(let offset):
-            var values = [
-                String(offset.width),
-                String(offset.height),
-                "offset-x\(String(offset.xOffset))",
-                "offset-y\(String(offset.yOffset))"
-            ]
-            if let value = offset.mode.value {
-                values.append(value)
-            }
-            return [URLQueryItem(name: ImageParameter.crop, value: values.joined(separator: ",") )]
-
-        }
+    }
+    ///The `overlay-pad` parameter allows you to add padding pixels to the edges of an overlay image.
+    ///You need to specify the relative URL of the image as value for this parameter.
+    ///See [Overlay Pad](https://www.contentstack.com/docs/developers/apis/image-delivery-api/#overlay-pad)
+    public func overlay(relativeUrl: String, padding: NSEdgeInsets) -> ImageTransform {
+        _ = self.overlay(relativeUrl: relativeUrl)
+        self.imageOperation.append(.overlayPad(padding))
+        return self
+    }
+    ///The `pad` parameter lets you add extra pixels to the edges of an image.
+    ///This is useful if you want to add whitespace or border to an image.
+    ///The value for this parameter can be given in pixels or percentage.
+    ///See [Pad](https://www.contentstack.com/docs/developers/apis/image-delivery-api/#pad)
+    public func pad(_ padding: NSEdgeInsets) -> ImageTransform {
+           self.imageOperation.append(.pad(padding))
+           return self
     }
 }
