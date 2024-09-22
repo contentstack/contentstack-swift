937f7bac245919e2bfd146387eca2f9c61e88b93
Vikram Kalta <vikramkalta@Vikrams-MacBook-Pro.local>
2024-09-22 16:12:01 +0100

    fix: another workflow fix attempt 7

--- Package.swift
+++ Package.swift
@@ -20,9 +20,9 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/vkalta/contentstack-utils-swift.git", from: "1.3.0"),
+        .package(url: "https://github.com/vkalta/contentstack-utils-swift.git", from: "master"),
         // Dev dependencies
-        .package(url: "https://github.com/vkalta/DVR.git", from: "2.1.0")
+        .package(url: "https://github.com/vkalta/DVR.git", from: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
