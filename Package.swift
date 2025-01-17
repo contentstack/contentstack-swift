ad5ec307405b5eeac94fa2f367e14c3098ac0df3
reeshika-h <reeshika.hosmani@contentstack.com>
2025-01-17 17:54:36 +0530

    updated the url for swift utils sdk

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/contentstack/contentstack-utils-swift.git", from: "1.3.0"),
+        .package(url: "https://github.com/contentstack/contentstack-utils-swift.git", .branch("master")),
         // Dev dependencies
         .package(url: "https://github.com/contentstack/contentstack-swift-dvr.git", .branch("master"))
     ],
